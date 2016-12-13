63588b801631e043a348def7773fdb3880d4b913
vimniky <vimniky@gmail.com>
2016-12-13 16:08:44 +0800

    Clean up

--- red/runtime/nodes/registry/index.js
+++ red/runtime/nodes/registry/index.js
@@ -14,73 +14,73 @@
  * limitations under the License.
  **/
 
-var when = require("when");
-var fs = require("fs");
-var path = require("path");
+const when = require('when')
+const fs = require('fs')
+const path = require('path')
 
-var events = require("../../events");
-var registry = require("./registry");
-var loader = require("./loader");
-var installer = require("./installer");
+const events = require('../../events')
+const registry = require('./registry')
+const loader = require('./loader')
+const installer = require('./installer')
 
-var settings;
+var settings
 
 function init(runtime) {
-    settings = runtime.settings;
-    installer.init(runtime.settings);
-    loader.init(runtime);
-    registry.init(settings,loader);
+  settings = runtime.settings
+  installer.init(runtime.settings)
+  loader.init(runtime)
+  registry.init(settings,loader)
 }
 
 function load() {
-    registry.load();
-    return installer.checkPrereq().then(loader.load);
+  registry.load()
+  return installer.checkPrereq().then(loader.load)
 }
 
 function addModule(module) {
-    return loader.addModule(module).then(function() {
-        return registry.getModuleInfo(module);
-    });
+  return loader.addModule(module).then(function() {
+    return registry.getModuleInfo(module)
+  })
 }
 
 function enableNodeSet(typeOrId) {
-    return registry.enableNodeSet(typeOrId).then(function() {
-        var nodeSet = registry.getNodeInfo(typeOrId);
-        if (!nodeSet.loaded) {
-            return loader.loadNodeSet(registry.getFullNodeInfo(typeOrId)).then(function() {
-                return registry.getNodeInfo(typeOrId);
-            });
-        }
-        return when.resolve(nodeSet);
-    });
+  return registry.enableNodeSet(typeOrId).then(function() {
+    var nodeSet = registry.getNodeInfo(typeOrId)
+    if (!nodeSet.loaded) {
+      return loader.loadNodeSet(registry.getFullNodeInfo(typeOrId)).then(function() {
+        return registry.getNodeInfo(typeOrId)
+      })
+    }
+    return when.resolve(nodeSet)
+  })
 }
 
 module.exports = {
-    init:init,
-    load:load,
-    clear: registry.clear,
-    registerType: registry.registerNodeConstructor,
+  init:init,
+  load:load,
+  clear: registry.clear,
+  registerType: registry.registerNodeConstructor,
 
-    get: registry.getNodeConstructor,
-    getNodeInfo: registry.getNodeInfo,
-    getNodeList: registry.getNodeList,
+  get: registry.getNodeConstructor,
+  getNodeInfo: registry.getNodeInfo,
+  getNodeList: registry.getNodeList,
 
-    getModuleInfo: registry.getModuleInfo,
-    getModuleList: registry.getModuleList,
+  getModuleInfo: registry.getModuleInfo,
+  getModuleList: registry.getModuleList,
 
-    getNodeConfigs: registry.getAllNodeConfigs,
-    getNodeConfig: registry.getNodeConfig,
+  getNodeConfigs: registry.getAllNodeConfigs,
+  getNodeConfig: registry.getNodeConfig,
 
-    enableNode: enableNodeSet,
-    disableNode: registry.disableNodeSet,
+  enableNode: enableNodeSet,
+  disableNode: registry.disableNodeSet,
 
-    addModule: addModule,
-    removeModule: registry.removeModule,
+  addModule: addModule,
+  removeModule: registry.removeModule,
 
-    installModule: installer.installModule,
-    uninstallModule: installer.uninstallModule,
+  installModule: installer.installModule,
+  uninstallModule: installer.uninstallModule,
 
-    cleanModuleList: registry.cleanModuleList,
+  cleanModuleList: registry.cleanModuleList,
 
-    paletteEditorEnabled: installer.paletteEditorEnabled
-};
+  paletteEditorEnabled: installer.paletteEditorEnabled,
+}
